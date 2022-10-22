615e3d5cb89562a647d6245127ec21612cb15757
Ivan Gonzalez Gonzalez <60527338+Ivan6801@users.noreply.github.com>
2022-10-22 14:44:05 -0700

    Update index.jsx
    update to api rest version 2

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -10,7 +10,7 @@ function useCategoriesData () {
   useEffect(function () {
     setLoading(true)
     setTimeout(() => {
-      window.fetch('https://petgram-server-rsor.vercel.app/categories')
+      window.fetch('https://petgram-serve-jorge-vicuna.vercel.app/categories')
         .then(res => res.json())
         .then(response => {
           setCategories(response)
